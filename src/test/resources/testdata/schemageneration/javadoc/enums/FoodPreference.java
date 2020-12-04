78e00265d418eb847f1a4c4bc0e19ee791226711
Adam Retter <adam.retter@googlemail.com>
2020-12-04 22:53:34 +0100

    Update to JAXB 3 package

--- src/test/resources/testdata/schemageneration/javadoc/enums/FoodPreference.java
+++ src/test/resources/testdata/schemageneration/javadoc/enums/FoodPreference.java
@@ -1,9 +1,9 @@
 package enums;
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlAttribute;
-import javax.xml.bind.annotation.XmlEnum;
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlAttribute;
+import jakarta.xml.bind.annotation.XmlEnum;
 
 /**
  * Simple enumeration example defining some Food preferences.
