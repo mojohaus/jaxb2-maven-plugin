4a200748db500e409e10015b3dedebe347f98ce2
Olivier Lamy <olamy@apache.org>
2025-10-21 14:58:39 +1000

    Plugin jdk minimum upgrade Jdk 11 as Upgrade jaxb runtime to 4.0.6 need jdk11 minimum (#380)
    * Upgrade jaxb runtime to 4.0.6
    * jaxb 4 is java 11 minimum
    
    ---------
    
    Signed-off-by: Olivier Lamy <olamy@apache.org>

--- src/main/java/org/codehaus/mojo/jaxb2/javageneration/AbstractJavaGeneratorMojo.java
+++ src/main/java/org/codehaus/mojo/jaxb2/javageneration/AbstractJavaGeneratorMojo.java
@@ -587,8 +587,6 @@ public abstract class AbstractJavaGeneratorMojo extends AbstractJaxbMojo {
                 // Refresh the BuildContext
                 getBuildContext().refresh(targetXsdDirectory);
             }
-        } catch (MojoExecutionException e) {
-            throw e;
         } catch (NoSchemasException e) {
             if (failOnNoSchemas) {
                 throw new MojoExecutionException("", e);
